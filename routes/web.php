d30d224daad8078321e248481fec02040e2083a0
lnwchok <sitthichok.punlabpho@gmail.com>
2017-02-21 01:23:49 +0700

    Try for Excel link

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-
+// use Excel;
 
 Route::get('/', function () {
 
@@ -9,5 +9,19 @@ Route::get('/', function () {
 
 Route::get('/bqreqs', 'BqreqsController@index');
 
+Route::get('/excels', function () {
 
+	$path = '/home/mps0229/Desktop/test.xls';
+
+	$data = Excel::load($path, function($reader) { })
+		->get(array('a1','b1'));
+
+	// $title = $data->getTitle();
+
+	// $data->dd();
+
+	return var_dump($data);
+
+	// return view('test', compact('data'));
+});
 
