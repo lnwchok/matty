8a70721e9ac48cb3b966bd5a3395b86cfc715fcb
lnwchok <sitthichok.punlabpho@gmail.com>
2017-02-21 14:22:30 +0700

    Again

--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,17 @@ Route::get('/excels', function () {
 
 	$path = '/home/mps0229/Desktop/test.xls';
 
-	$data = Excel::load($path, function($reader) { })
-		->get(array('a1','b1'));
+	$data = Excel::load($path, function($reader) {
+		$sheet = $reader->sheet(0);
+		$result = $sheet->get(array('a1','b1'));
+
+	 })->get();
 
 	// $title = $data->getTitle();
 
 	// $data->dd();
 
-	return var_dump($data);
+	return var_dump($result);
 
 	// return view('test', compact('data'));
 });
